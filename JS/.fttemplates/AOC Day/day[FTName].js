a6adefe79e36557a5116f2901869071677e53a6a
Tony Hunt <thunt@findmypast.com>
2022-12-04 12:37:04 +0000

    Created Template for AOC DayXX files

---
+++ JS/.fttemplates/AOC Day/day[FTName].js
@@ -0,0 +1,2 @@
+module.exports.part1 = (input) => 0;
+module.exports.part2 = (input) => 0;
---
+++ JS/.fttemplates/AOC Day/day[FTName].test.js
@@ -0,0 +1,22 @@
+const { test } = require("@jest/globals");
+const { part1, part2 } = require("./day01");
+const Input = require("./input");
+const EXAMPLE_INPUT = [];
+
+test("Part 1 Example", () => {
+  expect(part1(EXAMPLE_INPUT)).toBe(0);
+});
+
+test.skip("Part 1", () => {
+  const input = new Input([FTName]).fromLines().get();
+  expect(part1(input)).toBe(0);
+});
+
+test.skip("Part 2 Example", () => {
+  expect(part2(EXAMPLE_INPUT)).toBe(0);
+});
+
+test.skip("Part 2", () => {
+  const input = new Input([FTName]).fromLines().get();
+  expect(part2(input)).toBe(0);
+});
